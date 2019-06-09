13382526f5f8ca462f73c6ef598049deefd2f62d
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-08 22:42:11 -0300

    Criação da classe ExercicioDAO e da sua interface correspondente IExercicioDAO, faltando apenas o código dos métodos implementados.

---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/AlimentoDAO.java
@@ -0,0 +1,40 @@
+package br.edu.ifsc.cds.DAO;
+
+import java.util.List;
+
+import br.edu.ifsc.cds.DAO.intefaces.IAlimentoDAO;
+import br.edu.ifsc.cds.classes.domain.Alimento;
+
+public class AlimentoDAO implements IAlimentoDAO {
+
+	@Override
+	public void create(Alimento alimento) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	@Override
+	public List<Alimento> retrieveAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Alimento retrieve(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Alimento alimento) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
