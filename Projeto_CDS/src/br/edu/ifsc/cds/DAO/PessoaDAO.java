6e5269a7f47d36b5c1601f3faf9e38494db5af07
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-08 22:54:04 -0300

    Criaação da classe PessoaDAO e implementação da sua Interface.

---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/PessoaDAO.java
@@ -0,0 +1,40 @@
+package br.edu.ifsc.cds.DAO;
+
+import java.util.List;
+
+import br.edu.ifsc.cds.DAO.intefaces.IPessoaDAO;
+import br.edu.ifsc.cds.classes.domain.Pessoa;
+
+public class PessoaDAO implements IPessoaDAO{
+
+	@Override
+	public void create(Pessoa pessoa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Pessoa> retrieveAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pessoa retrieve(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Pessoa pessoa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/intefaces/IPessoaDAO.java
@@ -0,0 +1,18 @@
+package br.edu.ifsc.cds.DAO.intefaces;
+
+import java.util.List;
+
+import br.edu.ifsc.cds.classes.domain.Pessoa;
+
+public interface IPessoaDAO {
+
+	public void create(Pessoa pessoa);
+	
+    public List<Pessoa> retrieveAll();
+		
+	public Pessoa retrieve(Integer id);
+		
+	public void update(Pessoa pessoa);
+		
+	public void delete(Integer id);
+}
