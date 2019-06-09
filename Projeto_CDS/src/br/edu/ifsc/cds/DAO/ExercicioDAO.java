b08eeaac1cc2a49bb54df9c38cb9f69558e3b0b4
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-08 22:48:42 -0300

    Criação da classe ExercicioDAO e implentação da sua Inteface

---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/ExercicioDAO.java
@@ -0,0 +1,40 @@
+package br.edu.ifsc.cds.DAO;
+
+import java.util.List;
+
+import br.edu.ifsc.cds.DAO.intefaces.IExercicioDAO;
+import br.edu.ifsc.cds.classes.domain.Exercicio;
+
+public class ExercicioDAO implements IExercicioDAO{
+
+	@Override
+	public void create(Exercicio exercicio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Exercicio> retrieveAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Exercicio retrieve(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Exercicio exercicio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
