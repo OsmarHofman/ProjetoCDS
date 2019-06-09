926db06a3f72a16f679cf009506e72ce4f173117
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-08 22:57:12 -0300

    Criação da classe RotinaDAO e implementação da sua Interface

---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/RotinaDAO.java
@@ -0,0 +1,40 @@
+package br.edu.ifsc.cds.DAO;
+
+import java.util.List;
+
+import br.edu.ifsc.cds.DAO.intefaces.IRotinaDAO;
+import br.edu.ifsc.cds.classes.domain.Rotina;
+
+public class RotinaDAO implements IRotinaDAO{
+
+	@Override
+	public void create(Rotina rotina) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Rotina> retrieveAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rotina retrieve(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Rotina rotina) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/intefaces/IRotinaDAO.java
@@ -0,0 +1,18 @@
+package br.edu.ifsc.cds.DAO.intefaces;
+
+import java.util.List;
+
+import br.edu.ifsc.cds.classes.domain.Rotina;
+
+public interface IRotinaDAO {
+
+	public void create(Rotina rotina);
+	
+    public List<Rotina> retrieveAll();
+		
+	public Rotina retrieve(Integer id);
+		
+	public void update(Rotina rotina);
+		
+	public void delete(Integer id);
+}
