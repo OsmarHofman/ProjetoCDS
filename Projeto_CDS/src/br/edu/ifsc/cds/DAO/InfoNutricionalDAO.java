fa9e635a0cc23b2dd3a9b8189f6bf871f1706417
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-08 22:52:30 -0300

    Criação da classe InfoNutricionalDAO e implementação da sua Inteface.

---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/InfoNutricionalDAO.java
@@ -0,0 +1,40 @@
+package br.edu.ifsc.cds.DAO;
+
+import java.util.List;
+
+import br.edu.ifsc.cds.DAO.intefaces.IinfoNutricionalDAO;
+import br.edu.ifsc.cds.classes.domain.InfoNutricional;
+
+public class InfoNutricionalDAO implements IinfoNutricionalDAO{
+
+	@Override
+	public void create(InfoNutricional infnutri) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<InfoNutricional> retrieveAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InfoNutricional retrieve(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(InfoNutricional infnutri) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Projeto_CDS/src/br/edu/ifsc/cds/DAO/intefaces/IinfoNutricionalDAO.java
@@ -0,0 +1,17 @@
+package br.edu.ifsc.cds.DAO.intefaces;
+
+import java.util.List;
+import br.edu.ifsc.cds.classes.domain.InfoNutricional;
+
+public interface IinfoNutricionalDAO {
+	
+	public void create(InfoNutricional infnutri);
+	
+    public List<InfoNutricional> retrieveAll();
+		
+	public InfoNutricional retrieve(Integer id);
+		
+	public void update(InfoNutricional infnutri);
+		
+	public void delete(Integer id);
+}
