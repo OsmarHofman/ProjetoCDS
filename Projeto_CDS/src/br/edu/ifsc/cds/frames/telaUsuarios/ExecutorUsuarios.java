9be1f692d569da4ee794aaad2853c2e282ea0757
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-18 22:48:29 -0300

    Criação da Tela de relação de Usuários

---
+++ Projeto_CDS/src/br/edu/ifsc/cds/frames/telaUsuarios/ExecutorUsuarios.java
@@ -0,0 +1,27 @@
+package br.edu.ifsc.cds.frames.telaUsuarios;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ExecutorUsuarios extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Usuarios.fxml"));
+			Scene scene = new Scene(root, 914, 546);
+			scene.getStylesheets().add(getClass().getResource("Usuarios.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}// C:\Users\luana\git\ProjetoCDS\Projeto_CDS\src\br\edu\ifsc\cds\frames\telaHistorico
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ Projeto_CDS/src/br/edu/ifsc/cds/frames/telaUsuarios/UsuariosController.java
@@ -0,0 +1,16 @@
+package br.edu.ifsc.cds.frames.telaUsuarios;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class UsuariosController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
