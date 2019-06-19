54da25d0987b3aa8a642bad241e4b3155c34397d
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-18 22:49:57 -0300

    Atualização da tela de Exercícios com aplicação de layout

--- Projeto_CDS/src/br/edu/ifsc/cds/frames/telaExercicio/ExercicioController.java
+++ Projeto_CDS/src/br/edu/ifsc/cds/frames/telaExercicio/ExercicioController.java
@@ -1,5 +1,16 @@
 package br.edu.ifsc.cds.frames.telaExercicio;
 
-public class ExercicioController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ExercicioController implements Initializable{
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
