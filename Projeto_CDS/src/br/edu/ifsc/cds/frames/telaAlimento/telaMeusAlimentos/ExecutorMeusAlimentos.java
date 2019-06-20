a1ed22fbc23f1f3cfcf5d1a2965bcc91ceac1c94
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-20 17:52:03 -0300

    Criação da tela de meus Alimentos.

---
+++ Projeto_CDS/src/br/edu/ifsc/cds/frames/telaAlimento/telaMeusAlimentos/ExecutorMeusAlimentos.java
@@ -0,0 +1,31 @@
+package br.edu.ifsc.cds.frames.telaAlimento.telaMeusAlimentos;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ExecutorMeusAlimentos extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("MeusAlimentos.fxml"));
+			Scene scene = new Scene(root, 1023, 651);
+			scene.getStylesheets().add(getClass().getResource("MeusAlimentos.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+	
+}
