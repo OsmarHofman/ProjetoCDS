b54bd6c96e7ffed0f9b12ff7f63224adb522e07c
Adriel Goulart <adrielgoulartdeabranjo@gmail.com>
2019-06-22 17:12:55 -0300

    Exclusão do atributo estático do EntityManagerFactory da classeExecutora

--- Projeto_CDS/src/br/edu/ifsc/cds/classes/domain/Executora.java
+++ Projeto_CDS/src/br/edu/ifsc/cds/classes/domain/Executora.java
@@ -1,23 +1,9 @@
 package br.edu.ifsc.cds.classes.domain;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import br.edu.ifsc.cds.DAO.AdminDAO;
-
 public class Executora {
 
-	public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("cds-jpa");
-
 	public static void main(String[] args) {
-
-		// Admin a1 = new Admin(null,"Adriel","adriel.g","popopopopo","1111111111",
-		// "Guatelama - São João udheu", "99191991");
-		AdminDAO da = new AdminDAO();
-		// da.create(a1);
-
-		da.delete(7);
-
+		
 	}
 
 }
